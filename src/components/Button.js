9402a416ea22c0f8a2ec3bf696c936061a0e49fb
emily-chen702 <76749402+emily-chen702@users.noreply.github.com>
2022-01-02 13:47:04 -0500

    fixed download button

--- src/components/Button.js
+++ src/components/Button.js
@@ -28,7 +28,7 @@ export default function Button({
 }) {
   return (
     <ButtonStyle outline={outline} className="button-wrapper">
-      <Link className="button" to={btnLink}>
+      <Link className="button" to={btnLink} target="_blank" download>
         {btnText}
       </Link>
     </ButtonStyle>
---
+++ src/components/Filter.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export default function Filter() {
+  const [category, setCategory] = useState(' ');
+
+  return (
+    <div>
+      <h1> filter bar thing </h1>
+    </div>
+  );
+}
